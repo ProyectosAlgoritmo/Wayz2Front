38800ba70494289651cc500b15672ae8a228bfd6
anarojasduque <anarojas_algoritmos.co#EXT#@nrestrepoprexus.onmicrosoft.com>
2024-09-02 10:52:04 -0400

    diseño menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,14 @@
-/* import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { LateralmenuComponent } from './components/shared/lateralmenu/lateralmenu.component';
 import { ToolbarComponent } from './components/shared/toolbar/toolbar.component';
 import { AuthService } from './services/auth.service';
 import { SharedStateService } from './services/shared-state.service';
-<<<<<<< HEAD
 import { CommonModule } from '@angular/common';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 import { PowerBiReportComponent } from './components/powerbi-report/powerbi-report.component'; // Asegúrate de importar el componente
-=======
-
-
-import { ActivatedRoute } from '@angular/router';
->>>>>>> a83cb02f1308727b0e637a1687326e825eca97de
 
 @Component({
   selector: 'app-root',
@@ -47,14 +41,9 @@ import { ActivatedRoute } from '@angular/router';
 export class AppComponent implements OnInit {
   title = 'Strategicview';
   isLogged = false;
-<<<<<<< HEAD
   sidenavWidth = 230;
   isVisibleMenu = true;
   showPowerBIReport = false;  // Define esta propiedad
-=======
-  sidenavWidth = 350;
-  isVisibleMenu = true; 
->>>>>>> a83cb02f1308727b0e637a1687326e825eca97de
 
   constructor(
     private authService: AuthService,
@@ -71,18 +60,7 @@ export class AppComponent implements OnInit {
     });
 
     this.sharedStateService.isExpanded$.subscribe(isExpanded => {
-<<<<<<< HEAD
-      this.sidenavWidth = isExpanded ? 230 : 60;
-=======
-
-      if(isExpanded)
-        { 
-          this.sidenavWidth = 300;
-        }
-      else{
-        this.sidenavWidth = 100;
-      }
->>>>>>> a83cb02f1308727b0e637a1687326e825eca97de
+      this.sidenavWidth = isExpanded ? 280 : 100;
     });
 
     this.sharedStateService.isVisibleMenu$.subscribe(isVisible => {
@@ -91,79 +69,4 @@ export class AppComponent implements OnInit {
     });
   }
 }
- */
-
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { LateralmenuComponent } from './components/shared/lateralmenu/lateralmenu.component';
-import { ToolbarComponent } from './components/shared/toolbar/toolbar.component';
-import { AuthService } from './services/auth.service';
-import { SharedStateService } from './services/shared-state.service';
-import { CommonModule } from '@angular/common';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { trigger, state, style, transition, animate } from '@angular/animations';
-import { PowerBiReportComponent } from './components/powerbi-report/powerbi-report.component';
-
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
-    RouterOutlet,
-    RouterLink,
-    RouterLinkActive,
-    MatSidenavModule,
-    MatSlideToggleModule,
-    CommonModule,
-    ToolbarComponent,
-    LateralmenuComponent,
-    PowerBiReportComponent  // Importa y utiliza el componente PowerBiReportComponent
-  ],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  animations: [
-    trigger('fadeInOut', [
-      state('void', style({ opacity: 0 })),
-      transition('void <=> *', [animate(300)]),
-    ]),
-    trigger('slideInOut', [
-      state('in', style({ transform: 'translateX(0)' })),
-      state('out', style({ transform: 'translateX(-100%)' })),
-      transition('in <=> out', [animate(300)])
-    ]),
-  ]
-})
-export class AppComponent implements OnInit {
-  title = 'Strategicview';
-  isLogged = false;
-  sidenavWidth = 230;
-  isVisibleMenu = true;
-  showPowerBIReport = false;  // Propiedad para controlar la visibilidad del reporte
-
-  constructor(
-    private authService: AuthService,
-    private sharedStateService: SharedStateService,
-    private cdr: ChangeDetectorRef
-  ) {}
-
-  ngOnInit() {
-    // Suscribirse a la autenticación del usuario
-    this.authService.isAuthenticated().subscribe(authStatus => {
-      this.isLogged = authStatus;
-      if (!this.isLogged) {
-        // Lógica para redirigir al usuario si no está autenticado
-      }
-    });
-
-    // Suscribirse al estado expandido del menú lateral
-    this.sharedStateService.isExpanded$.subscribe(isExpanded => {
-      this.sidenavWidth = isExpanded ? 230 : 60;
-    });
-
-    // Suscribirse a la visibilidad del menú
-    this.sharedStateService.isVisibleMenu$.subscribe(isVisible => {
-      this.isVisibleMenu = isVisible;
-      this.cdr.detectChanges();
-    });
-  }
-}
+ 
