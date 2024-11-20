34e7a3e65ea8bfcc8a05f88a2090522402a739c8
carlosrodriguezbautista <47306037+carlosrodriguezbautista@users.noreply.github.com>
2024-11-20 16:48:24 -0500

    cambio direccion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true, 
-    apiUrl: 'https://tks.iterumconnections.net/api',
-    //apiUrl: 'https://172.18.88.10:8080/api'
+    //apiUrl: 'https://tks.iterumconnections.net/api',
+    apiUrl: 'https://127.0.0.1:5215/api',
     firebase: {
         apiKey: "AIzaSyADks6IO7oQRGVDlsQQ-UmbB4kxXPrPabg",
         authDomain: "strategicview-ba3c4.firebaseapp.com",
